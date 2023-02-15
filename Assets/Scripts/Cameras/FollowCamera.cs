ecfb80f8a68454472d2a54f27af219f7627515ba
tom <tom.garne@uwe.ac.uk>
2023-02-15 16:25:47 +0000

    added week 3 part 1 and extra animations

--- Assets/Scripts/Cameras/FollowCamera.cs
+++ Assets/Scripts/Cameras/FollowCamera.cs
@@ -6,7 +6,11 @@ public class FollowCamera : MonoBehaviour
 {
 
     public GameObject target;
-    private Vector3 Offset;
+    public Vector3 Offset;
+
+    private float mouseX;
+    private float mouseY;
+    private float mouseZ;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +23,55 @@ public class FollowCamera : MonoBehaviour
         float desiredAngle = target.transform.eulerAngles.y;
         Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
 
-        transform.position = Vector3.Lerp(transform.position, target.transform.position + (rotation * Offset), 0.05f);
+        transform.position = Vector3.Lerp(transform.position, target.transform.position + (rotation * Offset), 0.01f);
 
         //transform.position = target.transform.position + (rotation * Offset);
         
         transform.LookAt(target.transform);
+
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        mouseZ = Input.GetAxis("Mouse ScrollWheel");
+
+        if (Input.GetMouseButton(1))
+        {
+            Offset = Quaternion.Euler(0, mouseX, 0) * Offset;
+        }
+
+
+        float angleBetween = Vector3.Angle(Vector3.up, transform.forward);
+   
+
+        if (Input.GetMouseButton(0))
+        {
+            if (((angleBetween > 100.0f) && (mouseY < 0)) || ((angleBetween < 140.0f) && (mouseY > 0)))
+             {
+
+                Vector3 LocalRight = target.transform.worldToLocalMatrix.MultiplyVector(transform.right);
+                Offset = Quaternion.AngleAxis(mouseY, LocalRight) * Offset;
+
+            }
+         
+
+
+        }
+
+
+        float dist = Vector3.Distance(target.transform.position, transform.position);
+        Debug.Log("dist: " + dist);
+
+
+
+        if((mouseZ > 0) && (dist < 10.0f))
+        {
+            Offset = Vector3.Scale(Offset, new Vector3(1.05f, 1.05f, 1.05f));
+        }
+
+        if((mouseZ < 0) && (dist > 1.6f))
+        {
+            Offset = Vector3.Scale(Offset, new Vector3(0.95f, 0.95f, 0.95f));
+        }
+
     }
    
 }
