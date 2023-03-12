453bfe7c323442b8bb69c351fc0b382375ab180c
tom <tom.garne@uwe.ac.uk>
2023-03-12 16:40:49 +0000

    half way through worksheet 6

--- Assets/Scripts/Cameras/FollowCamera.cs
+++ Assets/Scripts/Cameras/FollowCamera.cs
@@ -1,3 +1,64 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    public GameObject target;
+    public Vector3 offset;
+
+
+    private float mouseX;
+    private float mouseY;
+    private float mouseZ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+
+    private void LateUpdate()
+    {
+        float angleBetween = Vector3.Angle(Vector3.up, transform.forward);
+        float desiredAngleY = target.transform.eulerAngles.y;
+        //float desiredAngleX = target.transform.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(0, desiredAngleY, 0);
+
+        transform.position = target.transform.position + (rotation * offset);
+
+        transform.LookAt(target.transform);
+
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        //mouseZ = Input.GetAxis("Mouse ScrollWheel");
+
+        if (Input.GetMouseButton(1))
+        {
+            Debug.Log(mouseX);
+
+            offset = Quaternion.Euler(0, mouseX, 0) * offset;
+            Debug.Log(offset);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            if (((angleBetween > 90) && (mouseY < 0)) || ((angleBetween < 140) && (mouseY > 0)))
+            {
+                Vector3 LocalRight = target.transform.worldToLocalMatrix.MultiplyVector(transform.right);
+                offset = Quaternion.AngleAxis(mouseY, LocalRight) * offset;
+            }
+
+        }
+    }
+}
+
+
+
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -75,3 +136,5 @@ public class FollowCamera : MonoBehaviour
     }
    
 }
+
+*/
--- Assets/Scripts/Game Controller/SetUpCameras.cs
+++ Assets/Scripts/Game Controller/SetUpCameras.cs
@@ -7,6 +7,7 @@ public class SetUpCameras : MonoBehaviour
 
     public Camera FollowCam;
     public Camera StaticCam;
+    public Camera PipCam;
 
     // Start is called before the first frame update
     void Start()
@@ -15,9 +16,17 @@ public class SetUpCameras : MonoBehaviour
 
         FollowCam.enabled = true;
         StaticCam.enabled = false;
+        PipCam.enabled = false;
 
         PlayerCharacter.GetComponent<AudioListener>().enabled = true;
         StaticCam.GetComponent<AudioListener>().enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp("p")){
+            PipCam.enabled = !PipCam.enabled;
+        }
     }
 
 
